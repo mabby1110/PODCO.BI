24ab9eb6e2f7c2982ec9f51c79af4c8af41e952c
mabby1110 <rznns.mabby@gmail.com>
2025-12-02 12:38:00 -0600

    ajustar a dispositivos

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="page-content">
+    movil
+</div>
+<style>
+    .page-content {
+        height: 100vh;
+    }
+</style>
